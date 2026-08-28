72b02a826b8fcec8aa9be9ed2d34d1c27eaf2339
i <isomorphisms@sdf.org>
2026-08-27 23:53:35 -0400

    Name demo after verified printing year

--- plate-renderer/app/Main.hs
+++ plate-renderer/app/Main.hs
@@ -1,9 +1,9 @@
 module Main (main) where
 
 import Albers.Plate (writePlatePng, writePlatePpm)
-import Albers.PlateData (xv2Yale1971Scan)
+import Albers.PlateData (xv2YaleFourthPrinting1972Scan)
 
 main :: IO ()
 main = do
-  writePlatePng "xv-2-yale-1971.png" 300 xv2Yale1971Scan
-  writePlatePpm "xv-2-yale-1971.ppm" 300 xv2Yale1971Scan
+  writePlatePng "xv-2-yale-fourth-printing-1972.png" 300 xv2YaleFourthPrinting1972Scan
+  writePlatePpm "xv-2-yale-fourth-printing-1972.ppm" 300 xv2YaleFourthPrinting1972Scan
